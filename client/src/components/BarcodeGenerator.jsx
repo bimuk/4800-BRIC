4f30b38770f49aae153d78c13619a73737a0be29
K0725 <kyamaguchi@cpp.edu>
2024-06-26 11:12:27 -0400

    added barcode api using next-barcode library

--- client/src/components/BarcodeGenerator.jsx
+++ client/src/components/BarcodeGenerator.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import Barcode from 'react-barcode';
+import { useBarcode } from 'next-barcode';
 
-const BarcodeGenerator = ({ email }) => {
-  return (
-    <div>
-      <Barcode value={email} />
-    </div>
-  );
+const BarcodeGenerator = ()  => {
+  const { inputRef } = useBarcode({
+    value: 'next-barcode',
+    options: {
+      background: '#ffff00',
+    }
+  });
+  
+  return <img ref={inputRef} />;
 };
+
 
 export default BarcodeGenerator;
